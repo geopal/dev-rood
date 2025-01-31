d36b0574ea300e499b05463a0b204bbf1bc165fe
georgios.palaiologou <geopal2010@gmail.com>
2025-01-31 20:46:11 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `DevRood`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to my Gatsby site!">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        // src='https://www.lonestardigital.com/EOS_D30/Sample_Pix/D30_Img_3771(400x400_Crop).jpg'
+        src='../images/icon.png'
+        alt='Moon'
+        />
     </Layout>
   )
 }
